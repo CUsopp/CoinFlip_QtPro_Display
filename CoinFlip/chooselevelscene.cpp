d7d01cc0ac599f352dff69e9e7b81efb33bdca38
CUsopp <228683942@qq.com>
2024-07-30 10:11:43 +0800

    Swap ManWindow And ChooseWindow.

---
+++ CoinFlip/chooselevelscene.cpp
@@ -0,0 +1,61 @@
+#include "chooselevelscene.h"
+#include <QMenuBar>
+#include <QPainter>
+#include "mypushbutton.h"
+#include <QDebug>
+#include <QTimer>
+
+ChooseLevelScene::ChooseLevelScene(QWidget *parent) : QMainWindow(parent)
+{
+    //配置选择关卡场景
+    this->setFixedSize(320,588);
+
+    //设置图标
+    this->setWindowIcon(QPixmap(":/res/Coin0001.png"));
+
+    //设置标题
+    this->setWindowTitle("选择关卡场景");
+
+    //创建菜单栏
+    QMenuBar * bar = menuBar();
+    setMenuBar(bar);
+
+    //创建开始菜单
+    QMenu * startMenu = bar->addMenu("开始");
+
+    //创建退出 菜单项
+    QAction *  quitAction = startMenu->addAction("退出");
+
+    //点击退出 实现退出游戏
+    connect(quitAction,&QAction::triggered,[=](){
+        this->close();
+    });
+
+    //返回按钮
+    MyPushButton * backBtn = new MyPushButton(":/res/BackButton.png" , ":/res/BackButtonSelected.png");
+    backBtn->setParent(this);
+    backBtn->move(this->width() - backBtn->width() , this->height() - backBtn->height());
+
+    //点击返回
+    connect(backBtn,&MyPushButton::clicked,[=](){
+        //qDebug() << "点击了返回按钮";
+        //告诉主场景 我返回了，主场景监听ChooseLevelScene的返回按钮
+        //延时返回
+        QTimer::singleShot(500,this,[=](){
+            emit this->chooseSceneBack();
+        });
+    });
+}
+
+void ChooseLevelScene::paintEvent(QPaintEvent *)
+{
+    //加载背景
+    QPainter painter(this);
+    QPixmap pix;
+    pix.load(":/res/OtherSceneBg.png");
+    painter.drawPixmap(0,0,this->width(),this->height(),pix);
+
+    //加载标题
+    pix.load(":/res/Title.png");
+    painter.drawPixmap( (this->width() - pix.width())*0.5,30,pix.width(),pix.height(),pix);
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CoinFlip_QtPro_Display
 My QtProject Here!
+
+# 一款翻金币的小游戏~
