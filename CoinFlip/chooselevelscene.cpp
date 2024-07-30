c838ef7a921f6a42dd0b5889fb43741d71e15234
CUsopp <228683942@qq.com>
2024-07-30 11:40:00 +0800

    Swap ChooseWindow And GameWindow.

--- CoinFlip/chooselevelscene.cpp
+++ CoinFlip/chooselevelscene.cpp
@@ -4,6 +4,7 @@
 #include "mypushbutton.h"
 #include <QDebug>
 #include <QTimer>
+#include <QLabel>
 
 ChooseLevelScene::ChooseLevelScene(QWidget *parent) : QMainWindow(parent)
 {
@@ -45,6 +46,42 @@ ChooseLevelScene::ChooseLevelScene(QWidget *parent) : QMainWindow(parent)
             emit this->chooseSceneBack();
         });
     });
+
+    //创建选择关卡的按钮
+     for( int i = 0 ; i < 20 ;i ++)
+     {
+         MyPushButton * menuBtn = new MyPushButton(":/res/LevelIcon.png");
+         menuBtn->setParent(this);
+         menuBtn->move( 25 + i%4 * 70 , 130 + i/4 * 70  );
+
+         //监听每个按钮的点击事件
+         connect(menuBtn,&MyPushButton::clicked,[=](){
+             QString str = QString("您选择的是第 %1 关 ").arg( i + 1);
+             qDebug() <<str;
+
+             //进入到游戏场景
+             this->hide(); //将选关场景隐藏掉
+             play = new PlayScene(i+1); //创建游戏场景
+             play->show();//显示游戏场景
+
+             connect(play,&PlayScene::chooseSceneBack,[=](){
+                 this->show();
+                 delete play;
+                 play = NULL;
+             });
+         });
+
+         QLabel * label = new QLabel;
+         label->setParent(this);
+         label->setFixedSize(menuBtn->width(),menuBtn->height());
+         label->setText(QString::number(i+1));
+         label->move(25 + i%4 * 70 , 130 + i/4 * 70 );
+
+         //设置 label上的文字对齐方式 水平居中和 垂直居中
+         label->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+         //设置让鼠标进行穿透   51号属性
+         label->setAttribute(Qt::WA_TransparentForMouseEvents);
+     }
 }
 
 void ChooseLevelScene::paintEvent(QPaintEvent *)
